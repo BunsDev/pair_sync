01c7ac5e8ff40d8c27ae9009399cd3824e79af5d
0xKitsune <0xKitsune@protonmail.com>
2022-09-18 13:45:52 -0400

    adding usd filter

--- src/filter.rs
+++ src/filter.rs
@@ -1,8 +1,17 @@
-use std::collections::HashSet;
-
-use ethers::types::H160;
+use std::{collections::HashSet, sync::Arc};
 
 use crate::pair::Pair;
+use ethers::prelude::ContractError;
+use ethers::providers::{Http, Provider, ProviderError};
+use ethers::{prelude::abigen, types::H160};
+
+abigen!(
+IUniswapV2Pair,
+r#"[
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)
+    function token0() external view returns (address)
+]"#;
+);
 
 //Filters out pairs where the blacklisted address is the token_a address or token_b address
 pub fn filter_blacklisted_tokens(pairs: Vec<Pair>, blacklisted_addresses: Vec<H160>) -> Vec<Pair> {
@@ -52,8 +61,53 @@ pub fn filter_blacklisted_addresses(
     filtered_pairs
 }
 
+//Filter that removes pools with that contain less than a specified usd value
 #[allow(dead_code)]
-fn filter_pools_below_usd_threshold() {}
+pub async fn filter_pools_below_usd_threshold(
+    pairs: Vec<Pair>,
+    v2_factory_address: H160, //Uniswap v2 factory variant to look up prices
+    usd_weth_pair_address: H160,
+    weth_address: H160,
+    usd_value_threshold: f64,
+    provider: Arc<Provider<Http>>,
+) -> Result<Vec<Pair>, ProviderError> {
+    let mut filtered_pairs = vec![];
+
+    //Get USD/Weth price
+    let usd_weth_pair = IUniswapV2Pair::new(usd_weth_pair_address, provider);
+
+    let (reserve_0, reserve_1, _) = match usd_weth_pair.get_reserves().call().await {
+        Ok(result) => result,
+        Err(contract_error) => match contract_error {
+            ContractError::ProviderError(provider_error) => return Err(provider_error),
+            other => {
+                panic!(
+                    "Error when getting USD/Weth reserves for filter USD Threshold filter: {}",
+                    other.to_string()
+                )
+            }
+        },
+    };
+
+    for pair in pairs {
+
+        //Get token_a/Weth price
+
+        //Calculate token_a usd value
+
+        //Get token_b/Weth price
+
+        //Calculate token_b usd value
+
+        //Compare the sum of token_a and token_b usd value against the specified threshold
+    }
+    Ok(filtered_pairs)
+}
+
+//Filter that removes pools with that contain less than a specified weth value
+//
+#[allow(dead_code)]
+fn filter_pools_below_weth_threshold(weth_address: H160, weth_value_threshold: f64) {}
 
 //Filter to remove tokens that incorporate fees on transfer.
 //This filter determines fee on transfer tokens by simulating a transfer and checking if the recieved amount is less
